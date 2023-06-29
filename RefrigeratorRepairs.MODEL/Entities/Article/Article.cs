71a6f22d2a440caa68425b27cc8da01415a6debe
navidkhanjari <navidkhanjari72@gmail.com>
2023-06-29 10:48:12 +0430

    Add Data Annotaion

--- RefrigeratorRepairs.MODEL/Entities/Article/Article.cs
+++ RefrigeratorRepairs.MODEL/Entities/Article/Article.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RefrigeratorRepairs.MODEL.Entities.Article
 {
@@ -7,11 +8,20 @@ namespace RefrigeratorRepairs.MODEL.Entities.Article
         #region (Fields)
         public string ImageName { get; set; }
         public string ImageAlt { get; set; }
+
+        [Required]
         public string Title { get; set; }
+        
+        [Required]
         public string Description { get; set; }
+        
+        [Required]
         public string ShortDescription { get; set; }
+        
         public string MetaDescription { get; set; }
         public string MetaKeyword { get; set; }
+        
+        [Required]
         public string Slug { get; set; }
         public int Visit { get; set; }
         public DateTime CreatedDate { get; set; }
--- RefrigeratorRepairs.MODEL/Entities/Settings/SiteSetting.cs
+++ RefrigeratorRepairs.MODEL/Entities/Settings/SiteSetting.cs
@@ -1,13 +1,26 @@
-﻿namespace RefrigeratorRepairs.MODEL.Entities.Settings
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RefrigeratorRepairs.MODEL.Entities.Settings
 {
     public class SiteSetting : BaseEntity
     {
         #region (Fields)
+        [Required]
         public string Background { get; set; }
+
+        [Required]
         public string TextInBackground { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public string PhoneNumber { get; set; }
+
+        [Required]
         public string WhatWeDo { get; set; }
+
+        [Required]
         public string AboutUs { get; set; }
         #endregion
     }
