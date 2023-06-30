be8445efa420eb0f97cc28c17d5756a919e86831
navidkhanjari72 <navidkhanjari72@gmail.com>
2023-06-30 14:08:06 +0430

    Fix codes

--- RefrigeratorRepairs.UI/Startup.cs
+++ RefrigeratorRepairs.UI/Startup.cs
@@ -54,10 +54,12 @@ namespace RefrigeratorRepairs.UI
             }
             else
             {
-                app.UseStatusCodePagesWithReExecute("/ErrorHandler/{0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            app.UseStatusCodePagesWithReExecute("/ErrorHandler/{0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
